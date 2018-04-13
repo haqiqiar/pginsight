f55f39e60b9578fd4d4d6b950c43b80eb7be3c95
RIZ-PC\haqiqi <rzkhqq@gmail.com>
2018-04-13 08:35:56 +0200

    first

---
+++ publish/Scripts/modalForm.js
@@ -0,0 +1,47 @@
+﻿$(function () {
+    $.ajaxSetup({ cache: false });
+    $("a[data-modal]").on("click", function (e) {
+        $("#MyModalContent").load(this.href, function () {
+            $("#MyModal").modal({
+                //backdrop: 'static',
+                keyboard: false
+            }, 'show');
+            bindForm(this);
+        });
+        return false;
+    });
+});
+
+function bindForm(dialog) {
+    $('form', dialog).submit(function () {
+        var data = new FormData(this.form);
+        $.ajax({
+            url: this.action,
+            type: this.method,
+            data: new FormData(this),
+            processData: false,
+            contentType: false,
+            success: function (result) {
+                debugger
+                if (result.success) {
+                    $("#MyModal").modal('hide');
+                    toastr.success(result.message);
+                    //table.draw();
+                    window.location.href = result.Url;
+                    location.reload();
+                }
+                else {
+                    //$("#MyModalContent").html(result);
+                    $("#MyModal").modal('show');
+                    toastr.error(result.ErrorMessage);
+                    bindForm(dialog);
+                    //location.reload();
+                }
+            },
+            error: function (xml, message, text) {
+                toastr.error("Msg: " + message + ", Text: " + text);
+            }
+        });
+        return false;
+    });
+}
